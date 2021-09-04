8cabb64a2ed232ad882920b2d0c8e02daa25c5bc
Caio Morato <morato_dev@protonmail.com>
2021-09-04 15:52:09 -0300

    toggle theme css feito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 // vitals
 import React from 'react';
+// styles
+import { FiSun } from 'react-icons/fi';
+import { FaMoon } from 'react-icons/fa';
 
 function Header() {
   return (
     <header>
       <h2>Where in the world?</h2>
       <div>
-        <input type="checkbox" name="toggle-theme" id="toggle-theme" />
-        <label htmlFor="toggle-theme">
-          
+        <input type="checkbox" className="toggle-input" name="toggle-input" id="toggle-input" />
+        <label htmlFor="toggle-input" className="toggle-label">
+          <FiSun color="#f39c12" />
+          <FaMoon color="#f1c40f" />
+          <div className="ball"></div>
         </label>
       </div>
     </header>
