145959c4c4319ae8ed4b538b29159fc8ae5f5f6d
Caio Morato <morato_dev@protonmail.com>
2021-09-04 18:04:11 -0300

    filtro e campo de busca feitos e estilizados

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,36 @@
 // vitals
-import React from 'react';
+import React, { useContext } from 'react';
 // components
 import Header from '../components/Header';
+// context
+import MyContext from '../context/MyContext';
+// styles
+import { FaSearch } from 'react-icons/fa';
 
 function Homepage() {
+  const { darkTheme } = useContext(MyContext);
   return (
-    <main>
+    <main className={darkTheme ? 'main-dark' : 'main-light'}>
       <Header />
+      <section>
+        <div className="search-filter-container">
+          <div className="search-container">
+            <FaSearch />
+            <input type="text" placeholder="Search for a country..." />
+          </div>
+          <div className="filter-container">
+            <select name="filter-options" id="filter-options">
+              <option selected>Filter by Region</option>
+              <option value="Africa">Africa</option>
+              <option value="America">America</option>
+              <option value="Asia">Asia</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
+            </select>
+          </div>
+        </div>
+        <div className="countries-container">Countries List</div>
+      </section>
     </main>
   );
 }
