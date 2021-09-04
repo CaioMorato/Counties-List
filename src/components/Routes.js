66d8e71efd144f2c144aef1669e6d6b939cf775e
Caio Morato <morato_dev@protonmail.com>
2021-09-04 03:06:16 -0300

    Roteamento inicial adicionado

---
+++ src/components/Routes.js
@@ -0,0 +1,15 @@
+// vitals
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// components
+import Homepage from '../pages/Homepage';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+    </Switch>
+  );
+}
+
+export default Routes;
