4e1ba0f79fe9176b169d0b41756d7576a979cf41
Caio Morato <morato_dev@protonmail.com>
2021-09-05 18:48:32 -0300

    browserrouter ajustado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // vitals
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // components
 import App from './App';
 
